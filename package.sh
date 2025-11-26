5823cecedeec25d07b7be2fc954aa79813d3e821
Joshua Bell <inexorabletash@gmail.com>
2025-11-25 20:52:56 -0800

    Package script improvements

--- package.sh
+++ package.sh
@@ -5,16 +5,53 @@
 
 set -e
 
-PACKDIR=$(mktemp -d)
-IMGFILE="out/chip8.po"
+IMGFILE="out/chip8.2mg"
 VOLNAME="CHIP8"
 
+# cecho - "color echo"
+# ex: cecho red ...
+# ex: cecho green ...
+# ex: cecho yellow ...
+function cecho {
+    case $1 in
+        red)    tput setaf 1 ; shift ;;
+        green)  tput setaf 2 ; shift ;;
+        yellow) tput setaf 3 ; shift ;;
+    esac
+    echo -e "$@"
+    tput sgr0
+}
+
+# suppress - hide command output unless it failed; and if so show in red
+# ex: suppress command_that_might_fail args ...
+function suppress {
+    set +e
+    local result
+    result=$("$@")
+    if [ $? -ne 0 ]; then
+        cecho red "$result" >&2
+        exit 1
+    fi
+    set -e
+}
+
 rm -f "$IMGFILE"
-cadius CREATEVOLUME "$IMGFILE" "$VOLNAME" 140KB --quiet --no-case-bits > /dev/null
+suppress cadius CREATEVOLUME "$IMGFILE" "$VOLNAME" 800KB --quiet --no-case-bits
+
+PACKDIR=$(mktemp -d)
+trap "rm -rf $PACKDIR" EXIT
 
 add_file () {
-    cp "$1" "$PACKDIR/$2"
-    cadius ADDFILE "$IMGFILE" "/$VOLNAME" "$PACKDIR/$2" --quiet --no-case-bits > /dev/null
+  src="$1"
+  dir=$(dirname "$2")
+  path="/$VOLNAME"
+  if [ "$dir" != "." ]; then
+    path="$path/$dir"
+  fi
+  filename=$(basename "$2")
+
+  cp "$src" "$PACKDIR/$filename"
+  suppress cadius ADDFILE "$IMGFILE" "$path" "$PACKDIR/$filename" --quiet --no-case-bits
 }
 
 add_file "res/PRODOS.SYS" "ProDOS#FF0000"
@@ -22,23 +59,4 @@ add_file "out/chip8.system.SYS" "CHIP8.system#FF0000"
 add_file "out/chip8.system.SYS" "BASIS.system#FF0000"
 add_file "res/ibm_logo.ch8" "IBM.LOGO.CH8#060000"
 
-# From https://github.com/Timendus/chip8-test-suite
-# Not included in package, but good to try
-#add_file "res/1-chip8-logo.ch8" "CHIP8.LOGO.CH8#060000"
-#add_file "res/2-ibm-logo.ch8" "IBM.LOGO2.CH8#060000"
-#add_file "res/3-corax+.ch8" "CORAX.CH8#060000"
-#add_file "res/4-flags.ch8" "FLAGS.CH8#060000"
-#add_file "res/5-quirks.ch8" "QUIRKS.CH8#060000"
-#add_file "res/6-keypad.ch8" "KEYPAD.CH8#060000"
-#add_file "res/7-beep.ch8" "BEEP.CH8#060000"
-#add_file "res/8-scrolling.ch8" "SCROLLING.CH8#060000"
-
-# Fun stuff from https://johnearnest.github.io/chip8Archive/
-#add_file "res/snek.ch8" "SNEK.CH8#060000"
-#add_file "res/octojam1title.ch8" "OCTOJAM1.CH8#060000"
-#add_file "res/octojam2title.ch8" "OCTOJAM2.CH8#060000"
-#add_file "res/octojam6title.ch8" "OCTOJAM6.CH8#060000"
-
-rm -rf "$PACKDIR"
-
-cadius CATALOG "$IMGFILE"
+cadius CATALOG "$IMGFILE" | cut -c1-$(tput cols)
